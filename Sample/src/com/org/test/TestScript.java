51cb92e28af694056be0ce30edd681610ffe1aae
MGK <MGK@192.168.1.121>
2019-02-23 21:01:03 +0400

    inserted loop

--- Sample/src/com/org/test/TestScript.java
+++ Sample/src/com/org/test/TestScript.java
@@ -3,6 +3,8 @@ package com.org.test;
 public class TestScript {
 
 	public static void main(String args[]) {
-		System.out.println("hello word");
+		for(int i=0;i<=10;i++) {
+		System.out.println("hello word"+i);
+		}
 	}
 }
